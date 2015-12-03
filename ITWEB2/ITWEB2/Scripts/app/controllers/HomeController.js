4879f6626d0433c97039b18f692ea4b6ff384a2d
Line Musha <musha@hotmail.dk>
2015-12-03 12:14:23 +0100

    That thing Rasmus did

--- ITWEB2/ITWEB2/Scripts/app/controllers/HomeController.js
+++ ITWEB2/ITWEB2/Scripts/app/controllers/HomeController.js
@@ -1,8 +1,38 @@
-﻿var HomeController = function ($scope) {
+﻿var HomeController = function ($scope, $http) {
     $scope.models = {
         helloAngular: 'I work!'
     };
+
+    var callbackData = function(data) {
+        $scope.models.listOfFood = data;
+    }
+
+    var init = function() {
+        //$.ajax({
+        //    dataType: "json",
+        //    url: "/api/FoodStuffs",
+        //    data: data,
+        //    success: callbackData
+        //});
+
+        $http({
+            method: 'GET',
+            url: '/api/FoodStuffs'
+        }).then(function successCallback(response) {
+            callbackData(response.data)
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+
+
+    }
+
+    init();
 }
 
+
+
+
 // The $inject property of every controller (and pretty much every other type of object in Angular) needs to be a string array equal to the controllers arguments, only as strings
-HomeController.$inject = ['$scope'];
+HomeController.$inject = ['$scope','$http'];
